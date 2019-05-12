9d59456f565135f1f50d6ceb32a4155999858635
Fr0stbyteR <renshihong@hotmail.com>
2019-05-12 03:44:39 +0200

    console, resize right panel

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Menu, Icon, MenuItemProps, Header, Loader, Dimmer, Table } from "semantic-ui-react";
+import { Menu, Icon, MenuItemProps, Header, Loader, Dimmer, Table, Ref } from "semantic-ui-react";
 import * as monacoEditor from "monaco-editor/esm/vs/editor/editor.api";
 import Patcher from "../core/Patcher";
 import "./RightMenu.scss";
@@ -13,10 +13,20 @@ enum TPanels {
 class Console extends React.Component {
     props: { patcher: Patcher };
     state: { cached: TPatcherLog[] } = { cached: [] };
+    refTable = React.createRef<HTMLTableElement>();
     handleNewLog = (log: TPatcherLog) => {
+        let bottom = true;
+        let table: HTMLTableElement;
+        if (this.refTable.current) {
+            table = this.refTable.current;
+            if (table.scrollTop + table.clientHeight !== table.scrollHeight) bottom = false;
+        }
         const cached = this.state.cached;
         cached.push(log);
-        this.setState({ cached });
+        this.setState({ cached }, () => (table && bottom ? table.scrollTop = table.scrollHeight : undefined));
+    }
+    handleClear = () => {
+        this.setState({ cached: [] });
     }
     componentDidMount() {
         this.props.patcher.on("newLog", this.handleNewLog);
@@ -32,11 +42,20 @@ class Console extends React.Component {
             </Table.Row>
         ));
         return (
-            <Table inverted striped selectable unstackable size="small" compact="very">
-                <Table.Body>
-                    {logs}
-                </Table.Body>
-            </Table>
+            <>
+                <Ref innerRef={this.refTable}>
+                    <Table inverted celled striped selectable unstackable size="small" compact="very">
+                        <Table.Body>
+                            {logs}
+                        </Table.Body>
+                    </Table>
+                </Ref>
+                <Menu icon inverted size="mini">
+                    <Menu.Item onClick={this.handleClear}>
+                        <Icon name="delete" inverted />
+                    </Menu.Item>
+                </Menu>
+            </>
         );
     }
 }
@@ -74,12 +93,13 @@ class CodeEditor extends React.Component {
         return this.codeEditorJSX;
     }
     get code() {
-        return this.props.patcher.toFaustDspCode();
+        return this.props.patcher.props.mode === "faust" ? this.props.patcher.toFaustDspCode() : "";
     }
 }
 export default class RightMenu extends React.Component {
     props: { patcher: Patcher };
     state = { active: TPanels.None };
+    refDivPane = React.createRef<HTMLDivElement>();
     refCode = React.createRef<CodeEditor>();
     refConsole = React.createRef<Console>();
     handleItemClick = (e: React.MouseEvent<HTMLAnchorElement>, data: MenuItemProps) => {
@@ -93,6 +113,35 @@ export default class RightMenu extends React.Component {
             });
         }
     };
+    handleResizeMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+        const origin = { x: e.pageX, y: e.pageY };
+        const curWidth = this.refDivPane.current.getBoundingClientRect().width;
+        const panel = this.state.active;
+        const handleMouseMove = (e: MouseEvent) => {
+            e.stopPropagation();
+            e.preventDefault();
+            if (this.refDivPane.current && e.movementX) {
+                const width = curWidth - (e.pageX - origin.x);
+                if (width < 100) {
+                    this.setState({ active: TPanels.None });
+                } else {
+                    if (this.state.active === TPanels.None) this.setState({ active: panel });
+                    this.refDivPane.current.style.width = width + "px";
+                    if (this.state.active === TPanels.Code && this.refCode.current && this.refCode.current.codeEditor) {
+                        this.refCode.current.codeEditor.layout();
+                    }
+                }
+            }
+        };
+        const handleMouseUp = (e: MouseEvent) => {
+            e.stopPropagation();
+            e.preventDefault();
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        };
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    }
     componentWillMount() {
     }
     componentDidMount() {
@@ -110,7 +159,7 @@ export default class RightMenu extends React.Component {
                         <Icon name="code" color={this.state.active === TPanels.Code ? "teal" : "grey"} inverted />
                     </Menu.Item>
                 </Menu>
-                <div id="right-pane" hidden={this.state.active === TPanels.None}>
+                <div id="right-pane" hidden={this.state.active === TPanels.None} ref={this.refDivPane}>
                     <Header as="h5" inverted color="grey" content={this.state.active} />
                     <div hidden={this.state.active !== TPanels.Code}>
                         <CodeEditor { ...this.props } ref={this.refCode} />
@@ -119,6 +168,7 @@ export default class RightMenu extends React.Component {
                         <Console { ...this.props } ref={this.refConsole} />
                     </div>
                 </div>
+                <div className="resize-handler resize-handler-w" onMouseDown={this.handleResizeMouseDown} hidden={this.state.active === TPanels.None}></div>
             </>
         );
     }
