62cdfae0084ea35cedbba829531ea94ea4f5ce66
Fr0stbyteR <renshihong@hotmail.com>
2019-11-16 00:44:03 +0800

    for

--- src/core/objects/Std.tsx
+++ src/core/objects/Std.tsx
@@ -54,4 +54,77 @@ class print extends StdObject<{}, { title: string }, [any], [], [string]> {
         });
     }
 }
-export default { print };
+class For extends StdObject<{}, { start: number; end: number; step: number }, [Bang, number, number, number], [number], [number, number, number?]> {
+    static get meta(): TMeta {
+        return {
+            ...super.meta,
+            description: "Number iterator",
+            inlets: [{
+                isHot: true,
+                type: "bang",
+                description: "Do iterations"
+            }, {
+                isHot: false,
+                type: "number",
+                description: "Set the starting point"
+            }, {
+                isHot: false,
+                type: "number",
+                description: "Set the end point (excluded)"
+            }, {
+                isHot: false,
+                type: "number",
+                description: "Set the step"
+            }],
+            outlets: [{
+                type: "number",
+                description: "Output all iterations one by one"
+            }],
+            args: [{
+                type: "number",
+                optional: false,
+                description: "The starting point"
+            }, {
+                type: "number",
+                optional: false,
+                description: "The end point (excluded)"
+            }, {
+                type: "number",
+                optional: true,
+                default: 1,
+                description: "The step"
+            }]
+        };
+    }
+    state = { start: 0, end: 0, step: 1 };
+    subscribe() {
+        super.subscribe();
+        this.on("preInit", () => {
+            this.inlets = 4;
+            this.outlets = 1;
+        });
+        this.on("updateArgs", (args) => {
+            this.state.start = +args[0] || 0;
+            this.state.end = +args[1] || 0;
+            this.state.step = +args[2] || 1;
+        });
+        this.on("inlet", ({ data, inlet }) => {
+            if (inlet === 0) {
+                if (data instanceof Bang) {
+                    const { start, end, step } = this.state;
+                    const times = (end - start) / step;
+                    if (!isFinite(times) || times < 0) {
+                        this.error(`Infinite loop from ${start} to ${end} with step ${step}.`);
+                        return;
+                    }
+                    for (let i = start; i < end; i += step) {
+                        this.outlet(0, i);
+                    }
+                }
+            } else if (inlet === 1) this.state.start = +data;
+            else if (inlet === 2) this.state.end = +data;
+            else if (inlet === 3) this.state.step = +data;
+        });
+    }
+}
+export default { print, for: For };
