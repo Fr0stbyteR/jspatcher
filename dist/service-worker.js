a5bd71bbb517e57d8e819bfbae646f0496452a90
Fr0stbyteR <renshihong@hotmail.com>
2020-05-31 17:10:05 +0800

    build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -96,7 +96,7 @@ define("./service-worker.js",['./workbox-5a7cf051'], function (workbox) { 'use s
   */
 
   workbox.setCacheNameDetails({
-    prefix: "0.0.1.1590685797737"
+    prefix: "0.0.1.1590913722468"
   });
   workbox.skipWaiting();
   workbox.clientsClaim();
@@ -291,7 +291,7 @@ define("./service-worker.js",['./workbox-5a7cf051'], function (workbox) { 'use s
     "revision": "90509a043949258c3c2d90e3e63740d2"
   }, {
     "url": "index.js",
-    "revision": "05d07c63b9fabea9f2d89e917161ffe6"
+    "revision": "ac9e55779f18b0ee945a071f77054190"
   }, {
     "url": "js/0a7a19440689db41e7b7.js",
     "revision": "22c9560326622216ce80dc0558c4921d"
