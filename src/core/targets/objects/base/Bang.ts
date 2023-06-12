3c797521f06f1842c8b287f489819a3a53115e86
CorvusPrudens <corvusprudens@gmail.com>
2023-06-12 16:38:05 -0600

    Removed outdated targets folder

--- src/core/targets/objects/base/Bang.ts
+++
@@ -1,8 +0,0 @@
-export default class Bang {
-    isBang = true;
-    toString() {
-        return "bang";
-    }
-}
-
-export const isBang = (x: any): x is Bang => typeof x === "object" && x?.isBang;
